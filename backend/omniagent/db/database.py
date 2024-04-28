89a611f900733bece8cbc5d77a2d835237f26843
Jowo Rinpoche <173829395+VividGen@users.noreply.github.com>
2024-04-28 19:20:18 +0800

    feat: Add Lifi widget

--- backend/omniagent/db/database.py
+++ backend/omniagent/db/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import create_database, database_exists
 
 from omniagent.conf.env import settings
-from omniagent.db.models import Base
+from omniagent.db.models import Base, User
 
 url = settings.BIZ_DB_CONNECTION
 
@@ -13,3 +13,8 @@ engine = create_engine(url, connect_args={"options": "-c timezone=utc"})
 Base.metadata.create_all(bind=engine)  # type: ignore
 
 DBSession = sessionmaker(bind=engine)
+
+
+if __name__ == "__main__":
+    with DBSession() as sess:
+        print(sess.query(User).all())
