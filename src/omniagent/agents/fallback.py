9c9ffdaeb38e836fbcf3375b1f4e8316e49afdef
Jowo Rinpoche <173829395+VividGen@users.noreply.github.com>
2024-06-08 17:09:00 +0800

    feat: Add memory support for langgraph

--- src/omniagent/agents/fallback.py
+++ src/omniagent/agents/fallback.py
@@ -8,17 +8,19 @@ from omniagent.conf.llm_provider import get_current_llm
 
 def fallback(state):
     logger.info("Running fallback agent")
+
     chat_template = ChatPromptTemplate.from_messages(
         [
             (
                 "system",
                 """
-You are omniagent, when other agents are not available, you will be used to answer user queries.
+You are omniagent!
 
 Your answer should be detailed and include puns or jokes where possible \
 And keep a lively, enthusiastic, and energetic tone, maybe include some emojis.
             """,
             ),
+            *state["messages"][0:-1],
             ("human", "{input}"),
         ]
     )
