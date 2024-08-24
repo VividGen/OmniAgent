c4664e7330daf9e915e901f46fed42e1d2c7773e
Jowo Rinpoche <173829395+VividGen@users.noreply.github.com>
2024-08-24 12:01:46 +0800

    chore: host to '0.0.0.0' in uvicorn run

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from loguru import logger
 if __name__ == "__main__":
     load_dotenv()
     logger.info("Starting OmniAgent")
-    uvicorn.run("omniagent.app:app", host="localhost", reload=False, port=8000)
+    uvicorn.run("omniagent.app:app", host="0.0.0.0", reload=False, port=8000)
