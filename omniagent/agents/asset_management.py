00e23900db45b0744202dc3646309eac51410382
Jowo Rinpoche <173829395+VividGen@users.noreply.github.com>
2024-09-21 13:48:42 +0800

    chores: Change the current model settings

--- omniagent/agents/asset_management.py
+++ omniagent/agents/asset_management.py
@@ -2,7 +2,6 @@ from dotenv import load_dotenv
 
 from omniagent.agents.agent_factory import create_agent
 from omniagent.conf.env import settings
-from omniagent.conf.llm_provider import get_current_llm
 from omniagent.executors.nft_balance_executor import NFTBalanceExecutor
 from omniagent.executors.swap_executor import SwapExecutor
 from omniagent.executors.token_balance_executor import TokenBalanceExecutor
@@ -10,27 +9,31 @@ from omniagent.executors.transfer_executor import TransferExecutor
 
 load_dotenv()
 
-executors = [SwapExecutor(), TransferExecutor()]
-if settings.COVALENT_API_KEY:
-    executors.extend([TokenBalanceExecutor(), NFTBalanceExecutor()])
 
-asset_management_agent = create_agent(
-    get_current_llm(),
-    executors,
-    """
-You are AssetManager, an AI assistant for crypto asset management. Your responsibilities include:
+def build_asset_management_agent(llm):
 
-1. Query and report on users' token balances
-2. Check and inform about users' NFT holdings
-3. Handle user requests to swap or transfer tokens
+    executors = [SwapExecutor(), TransferExecutor()]
+    if settings.COVALENT_API_KEY:
+        executors.extend([TokenBalanceExecutor(), NFTBalanceExecutor()])
 
-When interacting with users:
-- Provide accurate and detailed information
-- Maintain a friendly and enthusiastic tone
-- Use occasional puns or jokes to keep the conversation engaging
-- Include relevant emojis to enhance your messages
-- For privacy reasons, do not include address information when generating widgets
-
-Prioritize clarity and efficiency in your responses while keeping the interaction enjoyable for the user.
-""".strip(),
-)
+    asset_management_agent = create_agent(
+        llm,
+        executors,
+        """
+    You are AssetManager, an AI assistant for crypto asset management. Your responsibilities include:
+    
+    1. Query and report on users' token balances
+    2. Check and inform about users' NFT holdings
+    3. Handle user requests to swap or transfer tokens
+    
+    When interacting with users:
+    - Provide accurate and detailed information
+    - Maintain a friendly and enthusiastic tone
+    - Use occasional puns or jokes to keep the conversation engaging
+    - Include relevant emojis to enhance your messages
+    - For privacy reasons, do not include address information when generating widgets
+    
+    Prioritize clarity and efficiency in your responses while keeping the interaction enjoyable for the user.
+    """.strip(),
+    )
+    return asset_management_agent
