d5f228701f9006aad7750183584ecea15272c3f6
Jowo Rinpoche <173829395+VividGen@users.noreply.github.com>
2025-01-04 07:14:36 +0800

    chore: update api docs

--- omniagent/app.py
+++ omniagent/app.py
@@ -13,7 +13,14 @@ from omniagent.conf.env import settings
 from omniagent.router import openai_router, widget_router, health_router
 
 load_dotenv()
-app = FastAPI(title="OmniAgent", description="")
+app = FastAPI(
+    title="OmniAgent API",
+    description="OmniAgent is a framework for building AI applications leveraging the power of blockchains.",
+    license_info={
+        "name": "MIT",
+        "url": "https://github.com/vividgen/OmniAgent/blob/main/LICENSE",
+    },
+)
 
 app.add_middleware(
     CORSMiddleware,
@@ -51,8 +58,5 @@ async def global_exception_handler(request: Request, exc: Exception):
     logger.error(error_msg)
     return JSONResponse(
         status_code=500,
-        content={
-            "error": str(exc),
-            "traceback": traceback.format_exc()
-        }
+        content={"error": str(exc), "traceback": traceback.format_exc()},
     )
